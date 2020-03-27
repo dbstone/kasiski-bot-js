517030930ef1dd909181fba34a071b5db3a2adac
Devin Stone <bitq00@gmail.com>
2020-03-26 19:31:46 -0700

    tried with Eris, but speaking events are broken just like discord.py

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const Eris = require('eris')
 const ytdl = require('ytdl-core')
 
+var duckingEnabled = []
+var usersSpeaking = new Set()
+
 const bot = new Eris.CommandClient(process.env.DISCORD_TOKEN, {}, {
     description: "a Discord bot",
     owner: "Kasiski",
@@ -28,11 +31,31 @@ bot.registerCommand("play_test", (msg, args) => {
             console.log(err)
         })
 
+        connection.on('speakingStart', (userID) => {
+            if (duckingEnabled[msg.member.guild.id]) {
+                console.log(`speakingStart: ${String(usersSpeaking)}`)
+                usersSpeaking.add(userID)
+                connection.setVolume(0.5)
+            }
+        })
+        
+        connection.on('speakingStop', (userID) => {
+            if (duckingEnabled[msg.member.guild.id]) {
+                console.log(`speakingStop: ${String(usersSpeaking)}`)
+                usersSpeaking.delete(userId)
+                if (usersSpeaking.size === 0) {
+                    connection.setVolume(1.0)
+                }
+            }
+        })
+
+        connection.setVolume(1.0)
+
         if (connection.playing) {
             connection.stopPlaying()
         }
         
-        connection.play(ytdl(args[0], { filter: 'audioonly' }))
+        connection.play(ytdl(args[0], { filter: 'audioonly' }), { inlineVolume: 'true' } )
         connection.once('end', () => {
             bot.leaveVoiceChannel(msg.member.voiceState.channelID)
         })
@@ -54,9 +77,25 @@ bot.registerCommand("volume_test", (msg, args) => {
         return "You are not in a voice channel"
     }
 
+    if (isNaN(args[0]) || args[0] < 0 || args[0] > 1.0) {
+        return "Value must be a number between 0.0 and 1.0"
+    }
+    
     let connection = bot.voiceConnections.get(msg.member.guild.id)
     if (connection) {
-        connection.volume = args[0]
+        connection.setVolume(args[0])
+    }
+})
+
+bot.registerCommand("ducking", (msg, args) => {
+    let guild_id = msg.member.guild.id
+    if (duckingEnabled[guild_id])
+    {
+        duckingEnabled[guild_id] = false
+        return "Audio ducking disabled"
+    } else {
+        duckingEnabled[guild_id] = true
+        return "Audio ducking enabled"
     }
 })
 
